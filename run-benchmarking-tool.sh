7fc3bfbd2f08a79e76a38f3cea5bb257810bfab0
Gabriele Vernetti <gabriele.vernetti@protonmail.com>
2024-07-26 12:35:37 +0200

    include note into script

--- run-benchmarking-tool.sh
+++ run-benchmarking-tool.sh
@@ -9,6 +9,18 @@ DEFAULT_HASHRATE="10_000_000_000_000.0"
 DEFAULT_INTERVAL_A="30"
 DEFAULT_INTERVAL_C="60"
 
+# Display a note about the configurations
+bold=$(tput bold)
+underline=$(tput smul)
+reset=$(tput sgr0)
+echo ""
+echo -e "🚨 ${bold}Note:${reset}"
+echo -e "${bold}Configuration A:${reset} it runs every role, selecting txs and mining on custom jobs"
+echo -e "${bold}Configuration C:${reset} it doesn't run Job Declaration Protocol, so it will mine on Pool's block template"
+echo ""
+echo "Please have a look at https://stratumprotocol.org to better understand the Stratum V2 configurations and decide which one to benchmark."
+echo ""
+
 # Prompt user to select configuration (A or C) with default value
 read -p "Do you want to use configuration A or C? (Enter 'A' or 'C', default is 'A'): " CONFIG
 CONFIG=${CONFIG:-$DEFAULT_CONFIG}
@@ -21,6 +33,7 @@ if [[ "$CONFIG" != "A" && "$CONFIG" != "C" ]]; then
 fi
 
 # Prompt user to select network (mainnet, testnet3, or testnet4) with default value
+echo ""
 read -p "Do you want to use mainnet, testnet3, or testnet4? (Enter 'mainnet', 'testnet3', or 'testnet4', default is 'testnet4'): " NETWORK
 NETWORK=${NETWORK:-$DEFAULT_NETWORK}
 
@@ -31,6 +44,7 @@ if [[ "$NETWORK" != "mainnet" && "$NETWORK" != "testnet3" && "$NETWORK" != "test
 fi
 
 # Prompt user for hashrate to use for SV2 with default value
+echo ""
 read -p "Enter the hashrate for SV2 (e.g.: for 10 Th/s you need to enter 10_000_000_000_000.0, default is '10_000_000_000_000.0'): " hashrate
 hashrate=${hashrate:-$DEFAULT_HASHRATE}
 
@@ -41,18 +55,16 @@ if ! [[ "$hashrate" =~ ^[0-9_]+\.0$ ]]; then
 fi
 
 # Inform the user about the block template update interval and get the interval
+echo ""
 if [[ "$CONFIG" == "A" ]]; then
     echo "The SV1 pool used in the benchmarking tool will generate a new block template every 60 seconds."
-    echo "How often do you want your local Job Declarator Client (JDC) to produce updated templates? (default is '30'):"
+    read -p "How often do you want your local Job Declarator Client (JDC) to produce updated templates? (default is '30'): " SV2_INTERVAL
     DEFAULT_INTERVAL=$DEFAULT_INTERVAL_A
 else
     echo "The SV1 pool used in the benchmarking tool will generate a new block template every 60 seconds."
-    echo "How often do you want the SV2 pool to send updated block templates? This value will affect the bandwidth used. (default is '60'):"
+    read -p "How often do you want the SV2 pool to send updated block templates? This value will affect the bandwidth used. (default is '60'): " SV2_INTERVAL
     DEFAULT_INTERVAL=$DEFAULT_INTERVAL_C
 fi
-
-# Prompt user for block template update interval for SV2 with default value
-read -p "Enter the interval in seconds for updating the block template for SV2 (default is '$DEFAULT_INTERVAL'): " SV2_INTERVAL
 
 # Use default if no input is provided
 SV2_INTERVAL=${SV2_INTERVAL:-$DEFAULT_INTERVAL}
@@ -85,8 +97,9 @@ export SV2_INTERVAL
 docker-compose -f "docker-compose-config-${CONFIG}.yaml" up -d
 
 # Display final messages
-echo "Now point your miner to the SV1 pool: stratum+tcp://<host-ip-address>:3333"
-echo "And point your miner to the SV2 pool: stratum+tcp://<host-ip-address>:34255"
+echo ""
+echo "⛏️ ${underline}Now point your miner(s) to the SV1 setup:${reset} stratum+tcp://<host-ip-address>:3333"
+echo "⛏️ ${underline}And point your miner(s) to the SV2 setup:${reset} stratum+tcp://<host-ip-address>:34255"
 echo ""
 echo "You can access Grafana dashboard at the following link: http://localhost:3000/d/64nrElFmk/sri-benchmarking-tool 📊"
 echo ""
