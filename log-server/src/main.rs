38fd07f006fb1823ce97ad0a55f28a6b0c6f9166
Shourya742 <sshourya17@gmail.com>
2024-08-05 10:47:13 +0530

    Add readme content

--- log-server/src/main.rs
+++ log-server/src/main.rs
@@ -1,15 +1,15 @@
-use bollard::Docker;
 use bollard::container::ListContainersOptions;
+use bollard::Docker;
 use dotenv::dotenv;
+use log::{error, info};
 use reqwest::Client;
 use serde::Deserialize;
 use std::collections::HashMap;
 use std::env;
 use tar::Builder;
-use warp::{Filter, Rejection, Reply};
-use warp::hyper::Body;
 use warp::http::Response;
-use log::{error, info};
+use warp::hyper::Body;
+use warp::{Filter, Rejection, Reply};
 
 #[derive(Deserialize, Debug)]
 struct LokiResponse {
@@ -38,15 +38,13 @@ async fn main() {
     env_logger::init();
 
     let log_label = env::var("LOG_LABEL").expect("LOG_LABEL must be set");
-    let log_label = format!("logging={}",log_label);
+    let log_label = format!("logging={}", log_label);
     info!("Starting server with LOG_LABEL: {}", log_label);
 
-    let route = warp::path::end()
-        .and(warp::get())
-        .and_then(move || {
-            let log_label = log_label.clone();
-            async move { fetch_and_package_logs(&log_label).await }
-        });
+    let route = warp::path::end().and(warp::get()).and_then(move || {
+        let log_label = log_label.clone();
+        async move { fetch_and_package_logs(&log_label).await }
+    });
 
     warp::serve(route).run(([0, 0, 0, 0], 7420)).await;
 }
@@ -64,7 +62,7 @@ async fn fetch_and_package_logs(log_label: &str) -> Result<impl Reply, Rejection
                 .unwrap();
 
             Ok(response)
-        },
+        }
         Err(e) => {
             error!("Error fetching logs: {}", e);
             let response = Response::builder()
@@ -88,6 +86,23 @@ async fn fetch_and_package_logs_impl(log_label: &str) -> Result<Body, Box<dyn st
     {
         let mut tar_builder = Builder::new(&mut tar_data);
 
+        let readme_content = r#"
+### Download Logs from the Grafana Dashboard
+
+We’ve added a feature that allows you to download logs of all containers directly from the Grafana dashboard. Here’s how to use it:
+
+1. Navigate to the Grafana dashboard.
+2. Look for the 'Download Logs' button and click on it.
+3. The logs will be downloaded as a .tar file.
+
+To check logs of the containers and if facing any issues and want help, kindly share the logs in the [benchmarking channel on Discord](https://discord.com/channels/950687892169195530/1107964065936060467).
+"#;
+        let mut header = tar::Header::new_gnu();
+        header.set_size(readme_content.len() as u64);
+        header.set_mode(0o644);
+        header.set_cksum();
+        tar_builder.append_data(&mut header, "README.md", readme_content.as_bytes())?;
+
         for container in containers {
             info!("Fetching logs for container: {}", container);
             match fetch_logs(&client, &container).await {
@@ -96,10 +111,14 @@ async fn fetch_and_package_logs_impl(log_label: &str) -> Result<Body, Box<dyn st
                     header.set_size(logs.len() as u64);
                     header.set_mode(0o644);
                     header.set_cksum();
-                    if let Err(e) = tar_builder.append_data(&mut header, format!("{}.log", container), logs.as_bytes()) {
+                    if let Err(e) = tar_builder.append_data(
+                        &mut header,
+                        format!("{}.log", container),
+                        logs.as_bytes(),
+                    ) {
                         error!("Failed to append data for container {}: {}", container, e);
                     }
-                },
+                }
                 Err(e) => {
                     error!("Failed to fetch logs for container {}: {}", container, e);
                 }
@@ -114,7 +133,6 @@ async fn fetch_and_package_logs_impl(log_label: &str) -> Result<Body, Box<dyn st
     Ok(tar_file)
 }
 
-
 async fn get_containers(log_label: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     info!("Getting containers with label: {}", log_label);
 
@@ -128,15 +146,28 @@ async fn get_containers(log_label: &str) -> Result<Vec<String>, Box<dyn std::err
     });
 
     let containers = docker.list_containers(options).await?;
-    let container_names: Vec<String> = containers.into_iter().filter_map(|container| {
-        container.names.and_then(|names| names.get(0).map(|name| name.trim_start_matches('/').to_string()))
-    }).collect();
+    let container_names: Vec<String> = containers
+        .into_iter()
+        .filter_map(|container| {
+            container.names.and_then(|names| {
+                names
+                    .get(0)
+                    .map(|name| name.trim_start_matches('/').to_string())
+            })
+        })
+        .collect();
 
     Ok(container_names)
 }
 
-async fn fetch_logs(client: &Client, container: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let url = format!("http://loki:3100/loki/api/v1/query_range?query={{container=\"{}\"}}&limit=100000000", container);
+async fn fetch_logs(
+    client: &Client,
+    container: &str,
+) -> Result<String, Box<dyn std::error::Error>> {
+    let url = format!(
+        "http://loki:3100/loki/api/v1/query_range?query={{container=\"{}\"}}&limit=100000000",
+        container
+    );
     info!("Fetching logs from Loki: {}", url);
 
     let response: LokiResponse = client.get(&url).send().await?.json().await?;
@@ -149,7 +180,8 @@ async fn fetch_logs(client: &Client, container: &str) -> Result<String, Box<dyn 
 
     logs.sort_by(|a, b| a.0.cmp(&b.0));
 
-    let formatted_logs: String = logs.into_iter()
+    let formatted_logs: String = logs
+        .into_iter()
         .map(|(_, message)| format!("{}\n", message))
         .collect();
 
